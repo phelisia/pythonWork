2395c2a09d446fb6b36d54fe3ecd306de0bd2bf1
phelisa <phelisajeruto@gmail.com>
2022-06-10 22:24:38 +0300

    added functionality in classes account

--- bank.py
+++ bank.py
@@ -1,20 +1,57 @@
 class Account:
-    def __init__(self ,name,account_number,account_type,balance):
+    def __init__(self ,name,account_number,account_type):
         self.name=name
         self.account_number=account_number
         self.account_type=account_type
-        self.balance=balance
-    def withdraw (self):
-        withdrawn = float(input("Enter amount to be withdrawn: "))
-        if self.balance >= withdrawn:
-            self.balance -= withdrawn
-            print(f" your account name {self.name} with account number {self.account_number} and account type{self.account_type} and  balance {self.balance} you have withdrawn\n You Withdrew:", withdrawn)
+        self.balance=0
+        self.deposits=[]
+        self.withdrawals=[]
+
+        
+        
+    # def withdraw (self):
+    #     withdrawn = float(input("Enter amount to be withdrawn: "))
+    #     if self.balance >= withdrawn:
+    #         self.balance -= withdrawn
+    #         print(f" your account name {self.name} with account number {self.account_number} and account type{self.account_type} and  balance {self.balance} you have withdrawn\n You Withdrew:", withdrawn)
+    #     else:
+    #         print("\n Insufficient balance  ")
+    # def deposit (self):
+    #     withdrawn = float(input("Enter amount to be deposited: "))
+    #     if self.balance >= withdrawn:
+    #         self.balance += withdrawn
+    #         print(f" your account name {self.name} with account number {self.account_number} and account type{self.account_type} and  balance {self.balance} you have withdrawn\n You deposited:", withdrawn)
+    #     else:
+    #         print("\n Insufficient balance  ")    
+    # 
+    def deposit(self,amount):
+        if amount<=0:
+            return f"deposit amount must be greter than o"
         else:
-            print("\n Insufficient balance  ")
-    def deposit (self):
-        withdrawn = float(input("Enter amount to be withdrawn: "))
-        if self.balance >= withdrawn:
-            self.balance += withdrawn
-            print(f" your account name {self.name} with account number {self.account_number} and account type{self.account_type} and  balance {self.balance} you have withdrawn\n You deposited:", withdrawn)
+            self.balance+=amount
+            self.deposits.append(amount)
+            return f"'{self.balance}"
+    def withdrawa(self,amount):
+        transaction_fee=100
+        withdrawal_amount=amount+transaction_fee
+        if amount >self.balance:
+            return f"insufficient funds"
+        elif amount<=0:
+            return f"amount must be greater than 0"
         else:
-            print("\n Insufficient balance  ")        
+        
+            self.balance-=withdrawal_amount
+            self.withdrawals.append(amount)
+            
+            return f" hello {self.name}you have withdrawn{amount} your new balance is{self.balance}"     
+    def deposits_statement (self):
+        for depo in self.deposits:
+            print(f"You deposited{depo}")
+
+    def withdrawal_statements(self):
+        for witho in self.withdrawals:
+            print(f" you have withdrawn{witho}")     
+    def total_balance(self):
+        return self.balance
+
+                
---
+++ practice.py
@@ -0,0 +1,30 @@
+var="james bond"
+print(var[2::-1])
+sample={"jodi","jik","deg"}
+sample.add("vick")
+print(sample)
+varr="james" *4 *6
+print(varr)
+for i in range (10,15,1):
+    print(i,end=",")
+def t(n,m=6):
+    res=n*m
+    print(res)
+
+t(5,4)   
+
+x= 36/4*(3+2)*4+2
+print(x)
+sampleString ="google.com"
+
+for x in sample:
+    print(f"g{len(x)}")
+
+
+#copying in python
+my_foods = ['pizza', 'falafel', 'carrot cake']
+v =friend_foods = my_foods[:]
+print("My favorite foods are:")
+print(my_foods)
+print("\nMy friend's favorite foods are:")
+print(friend_foods)
