b06b4a83035988ede14f323f4c2de0a1358ac0ce
phelisa <phelisajeruto@gmail.com>
2022-06-19 22:39:25 +0300

    used lambda

--- bank.py
+++ bank.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 class Account:
     def __init__(self ,name,account_number,account_type):
         self.name=name
@@ -6,6 +7,10 @@ class Account:
         self.balance=0
         self.deposits=[]
         self.withdrawals=[]
+        self.transaction=100
+        self.newdict={}
+        self.loan_balance=0
+        
 
         
         
@@ -25,33 +30,70 @@ class Account:
     #         print("\n Insufficient balance  ")    
     # 
     def deposit(self,amount):
+        now=datetime.now()
         if amount<=0:
-            return f"deposit amount must be greter than o"
+            return f"deposit amount must be greater than 0 {now}"
         else:
+            self.narration=f" hello {self.name} you have deposited {amount} and your new balance is  {self.balance}  {now}"
             self.balance+=amount
             self.deposits.append(amount)
-            return f"'{self.balance}"
+            self.newdict["date"]=now
+            self.newdict["amount"]=amount
+            self.newdict["narration"]=self.narration
+            return f" hello {self.name} confirmed you have deposited {amount} and your new balance is {self.balance} on  {now}"
     def withdrawa(self,amount):
-        transaction_fee=100
-        withdrawal_amount=amount+transaction_fee
+        now=datetime.now()
+        withdrawal_amount=amount+self.transaction
         if amount >self.balance:
             return f"insufficient funds"
         elif amount<=0:
             return f"amount must be greater than 0"
         else:
-        
+            self.narration=f" hello  {self.name}  confirmed you have withdrawn {amount} your new balance is{self.balance} on{ now}" 
             self.balance-=withdrawal_amount
             self.withdrawals.append(amount)
+            self.newdict["date"]=now
+            self.newdict["amount"]=amount
+            self.newdict["narrations"]=self.narration
+
             
-            return f" hello {self.name}you have withdrawn{amount} your new balance is{self.balance}"     
+            return f" hello {self.name} you have withdrawn {amount} your new balance is {self.balance} on {now}"     
     def deposits_statement (self):
+        now=datetime.now()
         for depo in self.deposits:
-            print(f"You deposited{depo}")
+            print(f"You deposited {depo}  {now}")
 
     def withdrawal_statements(self):
-        for witho in self.withdrawals:
-            print(f" you have withdrawn{witho}")     
+         now=datetime.now()
+         for witho in self.withdrawals:
+            print(f" you have withdrawn {witho}  on {now}")     
     def total_balance(self):
-        return self.balance
+        now=datetime.now()
+        return f" your balance is {self.balance} on {now}"
+    def full_statement(self):
+        now=datetime.now()
+        full=[self.deposits+self.withdrawals]
+        for x in full:
+            print(f"{now}{x}")
+    def borrow(self,loan):
+        sum=0
+        for depo in self.deposits:
+            sum+=depo
+            print(sum)
+        if len(self.deposits)<10:
+            return f" you have more than 10 depoist and your loan is{loan}"
+        elif loan>100:
+            return f"you qualify for a loan"
+        elif :
+            pass
+        elif self.loan_balance==0:
+            pass
+
+            
+        else:
+            print("not qualified for a loan")    
+
+      
+
 
                 
--- practice.py
+++ practice.py
@@ -1,30 +1,56 @@
-var="james bond"
-print(var[2::-1])
-sample={"jodi","jik","deg"}
-sample.add("vick")
-print(sample)
-varr="james" *4 *6
-print(varr)
-for i in range (10,15,1):
-    print(i,end=",")
-def t(n,m=6):
-    res=n*m
-    print(res)
+# var="james bond"
+# print(var[2::-1])
+# sample={"jodi","jik","deg"}
+# sample.add("vick")
+# print(sample)
+# varr="james" *4 *6
+# print(varr)
+# for i in range (10,15,1):
+#     print(i,end=",")
+# def t(n,m=6):
+#     res=n*m
+#     print(res)
 
-t(5,4)   
+# t(5,4)   
 
-x= 36/4*(3+2)*4+2
-print(x)
-sampleString ="google.com"
+# x= 36/4*(3+2)*4+2
+# print(x)
+# sampleString ="google.com"
 
-for x in sample:
-    print(f"g{len(x)}")
+# for x in sample:
+#     print(f"g{len(x)}")
 
 
-#copying in python
-my_foods = ['pizza', 'falafel', 'carrot cake']
-v =friend_foods = my_foods[:]
-print("My favorite foods are:")
-print(my_foods)
-print("\nMy friend's favorite foods are:")
-print(friend_foods)
+# #copying in python
+# my_foods = ['pizza', 'falafel', 'carrot cake']
+# v =friend_foods = my_foods[:]
+# print("My favorite foods are:")
+# print(my_foods)
+# print("\nMy friend's favorite foods are:")
+# print(friend_foods)
+
+def greatest():
+    a=int(input("Enter number : "))
+    b=int(input("Enter number : "))
+    if a >=b :
+        print(a)
+    else:
+        print(b)
+greatest()
+
+#question2
+x,y,z=10 ,20, 30
+total=x+y+z
+print(total)
+kanamba=165
+
+sum=0
+for x in str(kanamba):
+    sum+=int(x)
+    print( sum)
+#swap in python
+x = 5
+y = 10
+y,x=x,y
+print("x =", x)
+print("y =", y)
